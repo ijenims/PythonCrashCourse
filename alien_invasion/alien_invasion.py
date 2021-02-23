a6039eac376a1866bd7ff3ed018f59811f4431ca
ijenims <77921755+ijenims@users.noreply.github.com>
2021-02-23 23:30:43 +0900

    Add files via upload

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -99,17 +99,30 @@ class AlienInvasion:
         ''' エイリアンの艦隊を作成する '''
         # エイリアンを1匹作成し、1列のエイリアンの数を求める
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # 最初の列のエイリアンを作成する
-        for alien_number in range(number_aliens_x):
-            # エイリアンを1匹作成し列の中に配置する
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+        # 画面に収まるエイリアンの列数を決定する
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                                (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # エイリアンの艦隊を作成する
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+
+    def _create_alien(self, alien_number, row_number):
+        ''' エイリアンを1匹作成し列の中に配置する '''
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
     
     def _update_screen(self):
